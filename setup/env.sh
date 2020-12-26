2c98b1e13e7293e68dd388e22a7bb9afb9faae3e
imssyang <imssyang@gmail.com>
2020-12-27 04:34:44 +0800

    update addressbook

--- setup/env.sh
+++ setup/env.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 export PROTOBUF_DISABLE_ENV=no
+export PROTOBUF_HOME=/opt/protobuf
 
-if [[ -z $CLASSPATH ]]; then
-  export CLASSPATH=.:/opt/protobuf/lib/protobuf-java-3.14.0.jar
-else
-  export CLASSPATH=.:/opt/protobuf/lib/protobuf-java-3.14.0.jar:$CLASSPATH
-fi
+#if [[ -z $CLASSPATH ]]; then
+#  export CLASSPATH=.:$PROTOBUF_HOME/lib/protobuf-java-3.14.0.jar:$PROTOBUF_HOME/lib/protobuf-java-util-3.14.0.jar
+#else
+#  export CLASSPATH=.:$PROTOBUF_HOME/lib/protobuf-java-3.14.0.jar:$PROTOBUF_HOME/lib/protobuf-java-util-3.14.0.jar:$CLASSPATH
+#fi
 
